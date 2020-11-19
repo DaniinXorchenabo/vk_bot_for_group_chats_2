a4ed10f5d1ffc2c62b9feca67a216e9072c882dc
DaniinXorchenabo <rkbcu@mail.ru>
2020-11-19 17:25:23 +0300

    fixing bugs in __init__

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-if True or __name__ == '__main__':
-    with open("main_with_flask.py", 'r', encoding='utf-8') as file:
-        text = file.read()
-        exec(text, {"__name__": '__main__'})
-    from time import sleep
-    print('******************')
-    while True:
-        sleep(1)
+# if True or __name__ == '__main__':
+#     with open("main_with_flask.py", 'r', encoding='utf-8') as file:
+#         text = file.read()
+#         exec(text, {"__name__": '__main__'})
+#     from time import sleep
+#     print('******************')
+#     while True:
+#         sleep(1)
